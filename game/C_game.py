b01ee494e63b6fb2326580ea213aae467f0576a1
Timothee <nobilettimothee@gmail.com>
2025-10-05 20:13:36 +0200

    menu

--- game/C_game.py
+++ game/C_game.py
@@ -1,15 +1,18 @@
 import pygame
-from etat import a_state
+from etat import State
 
 class Game:
     def __init__(self):
         # Set up the display (width, height)
         self.screen = pygame.display.set_mode((0, 0),pygame.FULLSCREEN)
+        self.screenSize = (self.screen.get_width(),self.screen.get_height())
 
         #Set up the clock for managing the frame rate
         self.fps = 60
         self.fpsClock = pygame.time.Clock()
         self.dt = 0 # Delta time between frames = devra faire *dt pour les mouvements
+
+        self.state = State(self.screen,self.screenSize)
 
     def run(self):
         running = True
@@ -19,7 +22,8 @@ class Game:
                 if event.type == pygame.QUIT:
                     running = False
     
-            a_state("menu",self.screen)
+            #Affiche ce qu'il doit être affiché en fonction du mode (reglage/menu/game)
+            self.state.a_state("menu")
 
             # Update the display
             pygame.display.flip()
--- game/etat.py
+++ game/etat.py
@@ -1,7 +1,21 @@
 import pygame
 
+class State:
 
-def a_state(state,screen):
+    def __init__(self,screen,screenSize):
+        self.screen = screen
+        self.Size = screenSize
 
-    if state == "menu":
-        pygame.draw.rect(screen, (255, 0, 0), (0, 0, 100, 100))
+
+    def a_state(self,state):
+
+        if state == "menu":
+
+
+            play = pygame.draw.rect(self.screen, (0, 255, 0), (self.Size[0]/3, 2*self.Size[1]/18, self.Size[0]/3, self.Size[1]/6))
+            parametre = pygame.draw.rect(self.screen, (50, 0, 58), (self.Size[0]/3, 7*self.Size[1]/18, self.Size[0]/3, self.Size[1]/6))
+            quit = pygame.draw.rect(self.screen, (255, 0, 0), (self.Size[0]/3, 12*self.Size[1]/18, self.Size[0]/3, self.Size[1]/6))
+            
+
+        else : 
+            pass
