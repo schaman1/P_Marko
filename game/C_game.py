92b24a59c635d84add02a9eab837c6befad4526f
Timothee <nobilettimothee@gmail.com>
2025-10-05 22:09:24 +0200

    menu

--- game/C_game.py
+++ game/C_game.py
@@ -4,7 +4,7 @@ from etat import State
 class Game:
     def __init__(self):
         # Set up the display (width, height)
-        self.screen = pygame.display.set_mode((0, 0),pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((pygame.display.Info().current_w, pygame.display.Info().current_h),pygame.FULLSCREEN | pygame.SCALED)
         self.screenSize = (self.screen.get_width(),self.screen.get_height())
 
         #Set up the clock for managing the frame rate
@@ -13,6 +13,7 @@ class Game:
         self.dt = 0 # Delta time between frames = devra faire *dt pour les mouvements
 
         self.state = State(self.screen,self.screenSize)
+        self.mod = "menu" #menu/reglage/game
 
     def run(self):
         running = True
@@ -21,9 +22,20 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.mod == "menu":
+                        if self.state.play.collidepoint(event.pos):
+                            print("Play button clicked")
+                        elif self.state.parametre.collidepoint(event.pos):
+                            print("Parametre button clicked")
+                        elif self.state.quit.collidepoint(event.pos):
+                            print("Quit button clicked")
+                            running = False
+
     
             #Affiche ce qu'il doit être affiché en fonction du mode (reglage/menu/game)
-            self.state.a_state("menu")
+            self.state.a_state(self.mod)
 
             # Update the display
             pygame.display.flip()
--- game/etat.py
+++ game/etat.py
@@ -5,17 +5,16 @@ class State:
     def __init__(self,screen,screenSize):
         self.screen = screen
         self.Size = screenSize
-
+        self.play = pygame.Rect(self.Size[0]/3, 2*self.Size[1]/18, self.Size[0]/3, self.Size[1]/6)
+        self.parametre = pygame.Rect(self.Size[0]/3, 7*self.Size[1]/18, self.Size[0]/3, self.Size[1]/6)
+        self.quit = pygame.Rect(self.Size[0]/3, 12*self.Size[1]/18, self.Size[0]/3, self.Size[1]/6)
 
     def a_state(self,state):
 
         if state == "menu":
-
-
-            play = pygame.draw.rect(self.screen, (0, 255, 0), (self.Size[0]/3, 2*self.Size[1]/18, self.Size[0]/3, self.Size[1]/6))
-            parametre = pygame.draw.rect(self.screen, (50, 0, 58), (self.Size[0]/3, 7*self.Size[1]/18, self.Size[0]/3, self.Size[1]/6))
-            quit = pygame.draw.rect(self.screen, (255, 0, 0), (self.Size[0]/3, 12*self.Size[1]/18, self.Size[0]/3, self.Size[1]/6))
             
-
+            pygame.draw.rect(self.screen, (0, 255, 0), self.play)
+            pygame.draw.rect(self.screen, (50, 0, 58), self.parametre)
+            pygame.draw.rect(self.screen, (255, 0, 0), self.quit)
         else : 
             pass
